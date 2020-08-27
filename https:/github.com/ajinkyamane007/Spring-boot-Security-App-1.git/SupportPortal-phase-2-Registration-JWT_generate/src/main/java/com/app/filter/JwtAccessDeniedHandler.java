e5b2d61eab0502a467a139a444e4981a3de18e47
ajinkyamane007 <ajinkyamane3996@gmail.com>
2020-08-27 13:40:00 +0530

    I] Token Provider - UserResource [create login method] + In UserResource [add DI - Authentication manager & JwtTokenProvider] + check name of 'authentcationManagerBean Bean' diff from 'authentcationManager DI' II] Filter - JwtAuthorizationFilter extends OncePerRequestFilter [ add DI JWTTokenProvider] JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint [If user is not Authenticated & Try to access the application - This method triggered] + class JwtAccessDeniedHandler  implements AccessDeniedHandler [ User Try to access Resource if he don't have enough Permission to it - This method triggered ] + UserRepository + UserServiceImp III] security configuration override 2 methods : SecurityConfiguration & configure . Add DI - [JwtAuthorizationFilter JwtAccessDeniedHandler JwtAuthenticationEntryPoint UserDetailsService bCryptPasswordEncoder] IV] Custom Exception

---
+++ https:/github.com/ajinkyamane007/Spring-boot-Security-App-1.git/SupportPortal-phase-2-Registration-JWT_generate/src/main/java/com/app/filter/JwtAccessDeniedHandler.java
@@ -0,0 +1,41 @@
+package com.app.filter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import com.app.constant.SecurityConstant;
+import com.app.domain.HttpResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class JwtAccessDeniedHandler  implements AccessDeniedHandler{
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		HttpResponse httpresponse = new HttpResponse(
+				HttpStatus.UNAUTHORIZED.value(),    // 403
+				HttpStatus.UNAUTHORIZED,            // 403, "Forbidden"
+				HttpStatus.UNAUTHORIZED.getReasonPhrase().toUpperCase(), //
+				SecurityConstant.ACCESS_DENIED_MESSAGE);   // You do not have permission to access this page
+		
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpStatus.UNAUTHORIZED.value() );
+		
+		OutputStream outputStream = response.getOutputStream();
+		ObjectMapper mapper=new ObjectMapper();
+		mapper.writeValue(outputStream, httpresponse);
+		outputStream.flush();			
+	}
+
+}
